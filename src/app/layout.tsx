e020b929d3d84aea9d642c6a6986948ff26ee646
Patrick Poss <hey@patrickposs.com>
2025-08-08 19:50:13 +0100

    Rename: Title and window label to Anonymous Survey

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Questions To Contemplate", 
-  description: "A design research experiment exploring existential thoughts",
+  title: "Anonymous Survey", 
+  description: "A design research experiment exploring anonymous existential thoughts",
 };
 
 export default function RootLayout({
