c1cda42c3f55b1a118b5080d59a4169000c454ce
Patrick Poss <hey@patrickposs.com>
2025-09-07 12:17:31 +0100

    perf: preload restart icon to avoid flicker in About menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" as="image" href="/shut_down.png" />
+      </head>
       <body className={msSans.variable}>
         <div
           id="bg-root"
